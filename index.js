6c2c0523a605eedc9938126f386323598c25d7cb
unknown <bahfatoumatambalou64@gmail.com>
2025-01-04 07:49:41 -0800

    liste de choses à faire

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ function validateForm(){
     const taskText = taskInput.value;
 
     newTask.innerText = taskText;
-
-    const completedBoutton = document.createElement('button');
-    completedBoutton.textContent = 'confirmer';
-    completedBoutton.onclick = function(){
-     taskListe.classList.toggle(newTask);
-            };
-            newTask.appendChild(completedBoutton);
-
-
+        
             const deleteButton = document.createElement('button');
             deleteButton.textContent = 'Supprimer';
             deleteButton.onclick = function(){
